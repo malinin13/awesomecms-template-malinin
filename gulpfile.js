164661791987ee7e1c70305b2fa1c7080d2ceaf3
Eugene <malinin13@gmail.com>
2014-01-25 13:34:14 +0100

    Header ready, css not moduled

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,42 @@
-var gulp            = require('gulp'),
+var lr              = require('tiny-lr'),
+    gulp            = require('gulp'),
     clean           = require('gulp-clean'),
     jade            = require('gulp-jade'),
     minifyCss       = require('gulp-minify-css'),
     rename          = require('gulp-rename'),
     sass            = require('gulp-sass'),
-    uglify          = require('gulp-uglify');
+    uglify          = require('gulp-uglify'),
+    livereload      = require('gulp-livereload'),
+    server          = lr(),
+    serverPort      = 63342;
 
+/** -S пиши для плагинов**/
 gulp.task('template:jade', function(){
 
     return gulp.src('./layouts/*.jade')
         .pipe(jade())
-        .pipe(gulp.dest('./assets/html'));
+        .pipe(gulp.dest('C:/OpenServer/domains/awesomecms/assets/html'));
 });
 
 gulp.task('css:sass', function () {
     gulp.src('./components/**/*.scss')
         .pipe(sass())
-        .pipe(gulp.dest('./assets/css'));
+        .pipe(gulp.dest('C:/OpenServer/domains/awesomecms/assets/css/'))
+        .pipe(livereload(server));
+});
+gulp.task('lr-server', function() {
+    server.listen(35729, function(err) {
+        if(err) return console.log(err);
+    });
 });
 
 gulp.task('default', function(){
-    gulp.run('template:jade', 'css:sass');
-
-    gulp.watch('./layouts/*.jade', function(){
-        gulp.run('template:jade');
-    });
+        gulp.run('lr-server');
+        gulp.run('template:jade', 'css:sass');
+        gulp.watch('./layouts/*.jade', function(){
+            gulp.run('template:jade');
+        });
+        gulp.watch('./components/**/*.scss', function(){
+            gulp.run('css:sass');
+        });
 });
